2fbb9cedb8904db6b83a57b988cba91a2c931edf
LuqyanaAryahiyya <aryahiyya@mhs.usk.ac.id>
2026-05-27 02:38:22 +0700

    Create basic_sorting.c

---
+++ basic_sorting.c
@@ -0,0 +1,36 @@
+#include "basic_sorting.h"
+#include "utils.h"
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swapInt(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        swapInt(&arr[i], &arr[min]);
+    }
+}
